08df756266760690f581911958a8f05076876520
RosyG <rmga2106@gmail.com>
2017-10-23 16:02:08 -0500

    Ejercicio sobre funciones dentro de objects

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var fila=[1,2,3,4];
-fila.shift();
-document.write(fila);
-alert('Hay que comer bien.')
+var lasPadawans={
+  name1: 'Viri',
+  name2: 'Eri',
+  name3: 'Ari',
+  name4: 'Sam',
+  name5: 'Nan',
+  caracteristicasAri: {altura:1.60, edad:27, calorCabello:'negro'},
+  VerdurasPreferidas:['jitomate', 'algas'],
+saludar: function(){
+    alert('¡Hola squad!');
+  }
+}
+console.log(lasPadawans.saludar);
